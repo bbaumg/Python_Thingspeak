d8482d0dfaa6b37fa1b1c1e496304853e7add2df
bbaumg <bbaumg@gmail.com>
2018-02-11 12:00:07 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='Python_Thingspeak',
-	version='v1.0.0',
+	version='1.0.1',
 	description='Driver for sending Thingspeak data',
 	url='https://github.com/bbaumg/Python_Thingspeak',
 	author='bbaumg',
