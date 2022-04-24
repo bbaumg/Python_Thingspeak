711c81dcfd2af2544fe776f7add760be818e6401
bbaumg <bbaumg@gmail.com>
2022-04-24 10:56:38 -0500

    updated code to run under python3

--- Thingspeak/Thingspeak.py
+++ Thingspeak/Thingspeak.py
@@ -136,6 +136,7 @@ class Thingspeak(object):
 			ts=self.tsRUL,
 		)
 		logging.debug("Options = " + str(options))
+		logging.debug("URL = " + str(url))
 		try:
 			results = requests.post(url, params=options)
 			if results.ok != True:
@@ -160,9 +161,16 @@ class Thingspeak(object):
 		"""returns the name of field when description is passed in"""
 		logging.info("Getting the field name " + str(name))
 		try:
-			fieldName = self.fields.keys()[self.fields.values().index(name)]
+			#fieldName = self.fields.keys()[self.fields.values().index(name)]
+			#fieldName = next(v for k, v in self.fields.items() if v == name )
+			for key, value in self.fields.items():
+				if value == name:
+					fieldName = key
 			logging.info("The field name for " + str(name) + " is " + str(fieldName))
 			return fieldName
+		except Exception as e:
+			logging.error(e)
 		except:
 			logging.error(str(name)+ " Field Name was not found")
+			print(f"Unexpected {err=}, {type(err)=}")
 			return False
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='Python_Thingspeak',
-	version='1.0.1',
+	version='1.1',
 	description='Driver for sending Thingspeak data',
 	url='https://github.com/bbaumg/Python_Thingspeak',
 	author='bbaumg',
