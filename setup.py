187f227306480ba5f7c87dfef9cdc83b37eda9e7
bbaumg <bbaumg@gmail.com>
2022-04-28 17:57:45 -0500

    rebuilt package for updated installer

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
-from setuptools import setup
+from setuptools 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
-setup(name='Python_Thingspeak',
+setuptools.setup(
+	name='Python_Thingspeak',
 	version='1.1',
 	description='Driver for sending Thingspeak data',
 	url='https://github.com/bbaumg/Python_Thingspeak',
 	author='bbaumg',
-	license='MIT',
-	packages=['Thingspeak'],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+  ],
+	package_dir={"": "src"}
+	packages=setuptools.find_packages(where="src"),
 	install_requires=['requests'],
 	zip_safe=False)
